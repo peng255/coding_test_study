447260d200b377941bb8ec7df3b30ba98d96d8c0
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-06 16:03:31 +0900

    Title: 더 긴 문자열, Time: 106ms, Memory: 10MB, Status: Passed - Codetree

--- 250906/더 긴 문자열/longer-string.java
+++ 250906/더 긴 문자열/longer-string.java
@@ -3,5 +3,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next().trim();
+        String b = sc.next().trim();
+
+        if ( a.length() > b.length() ){
+            System.out.println(a + " " + a.length());
+        } else if (a.length() == b.length()){
+            System.out.println("same");
+        } else {
+            System.out.println(b + " " + b.length());
+        }
     }
 }

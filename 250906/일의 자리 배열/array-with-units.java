388e7af1e23004fd39d0c4c9619cc853a62e717a
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-06 15:49:43 +0900

    Title: 일의 자리 배열, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

--- 250906/일의 자리 배열/array-with-units.java
+++ 250906/일의 자리 배열/array-with-units.java
@@ -10,7 +10,7 @@ public class Main {
         arr[0] = a;
         arr[1] = b;
         for(int i = 2; i < 10; i ++){
-            arr[i] = arr[i-2] + arr[i-1];
+            arr[i] = (arr[i-2] + arr[i-1]) % 10;
         }
 
         for(int i = 0; i < arr.length; i++){

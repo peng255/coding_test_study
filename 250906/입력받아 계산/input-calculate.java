9cbd3249fc0233a557db12fd11596088b775bf50
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-06 15:20:04 +0900

    Title: 입력받아 계산, Time: 115ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250906/입력받아 계산/input-calculate.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = Integer.parseInt(sc.next());
+
+        System.out.println(a+2);
+    }
+}

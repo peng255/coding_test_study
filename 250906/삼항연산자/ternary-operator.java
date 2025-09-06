f85ceddd7ac80c114e8c6ed496b83fc47dd50516
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-06 22:40:23 +0900

    Title: 삼항연산자, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250906/삼항연산자/ternary-operator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println( num == 100 ? "pass" : "failure");
+    
+    }
+}

8d0832dee73ee7b0270c2270ff7e28f97a2eed14
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-07 21:43:53 +0900

    Title: 시력 검사, Time: 144ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250907/시력 검사/eye-test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if( a>=1.0 && b>=1.0){
+            System.out.println("High");
+        } else {
+            System.out.println("Low");
+        }
+
+    }
+}

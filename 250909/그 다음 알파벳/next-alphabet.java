876119518cb645b2acaf1a23267c6d516605adce
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-09 14:30:36 +0900

    Title: 그 다음 알파벳, Time: 117ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        char c = (new Scanner(System.in)).next().charAt(0);
+
+        if(c=='z'){
+            System.out.print('z');
+        } else {
+            System.out.print((char)(c + 1));
+        }
+    }
+}

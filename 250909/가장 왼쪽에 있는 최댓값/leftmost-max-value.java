3b7068dab31e8cf332bf12fc581d2013b17a077b
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-09 09:24:27 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 202ms, Memory: 11MB, Status: Passed - Codetree

--- 250909/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 250909/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -22,7 +22,7 @@ public class Main {
         while(true){
             
             // 이전 max 인덱스는 포함하지 않는 범위에서 최대인덱스 찾기
-            end = find(end-1);
+            end = find(end);
             System.out.print(end+1 + " ");
 
             if(end==0){
@@ -32,7 +32,7 @@ public class Main {
         
     }
 
-    // 0~end 범위 안에서 max 인덱스 찾음
+    // 0~end-1 범위 안에서 max 인덱스 찾음
     public static int find(int end){
         int max = Integer.MIN_VALUE;
         int maxIdx = 0;

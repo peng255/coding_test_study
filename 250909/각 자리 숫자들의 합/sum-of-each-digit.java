a33e209ec7f18edd0f618532b386beb2184ccf8c
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-09 14:50:34 +0900

    Title: 각 자리 숫자들의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250909/각 자리 숫자들의 합/sum-of-each-digit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        int n = (new Scanner(System.in)).nextInt();
+
+        String s = Integer.toString(n);
+
+        int sum = 0;
+        for (int i = 0; i < s.length(); i ++){
+            // char -> String 바꿀 때 String.valueOf(_) 쓰기
+            String letter = String.valueOf(s.charAt(i))
+            sum += Integer.parseInt();
+        }
+
+        System.out.print(sum);
+    }
+}

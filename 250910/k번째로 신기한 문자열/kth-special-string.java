602ff9e8d00ee973e9f613621d5ebc39394dbacb
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-10 09:39:01 +0900

    Title: k번째로 신기한 문자열, Time: 148ms, Memory: 11MB, Status: Runtime Error - Codetree

---
+++ 250910/k번째로 신기한 문자열/kth-special-string.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String t = sc.next();
+        String[] words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = sc.next();
+        }
+        // Please write your code here.
+
+        Arrays.sort(words);
+
+        int idxcnt = 1;
+        int len = t.length();
+        for (int i = 0; i < n; i ++){
+            if ( (words[i].substring(0,len)).equals(t) ){
+                if (idxcnt == k){
+                    System.out.print(words[i]);
+                    return;
+                } else {
+                    idxcnt ++;
+                }
+            }
+        }
+    }
+}

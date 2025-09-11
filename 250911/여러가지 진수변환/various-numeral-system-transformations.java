4e222569875e864e2a61ce9fdafbb4a82a09ffa7
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-11 18:54:53 +0900

    Title: 여러가지 진수변환, Time: 123ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250911/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+
+        int arr[] = new int[n/2];
+
+        int cnt=0;
+        while(true){
+            if(n < b){
+                arr[cnt++] = n;
+                break;
+            }
+
+            arr[cnt++] = n%b;
+            n /= b;
+
+        }
+
+        for (int i = cnt-1; i >=0; i --){
+            System.out.print(arr[i]);
+        }
+    }
+}

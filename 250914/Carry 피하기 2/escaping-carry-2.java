cb3f4770b4da09756204fa25156c2bb72b9a1255
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-14 00:19:09 +0900

    Title: Carry 피하기 2, Time: 131ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250914/Carry 피하기 2/escaping-carry-2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static boolean checkNotCarry(int a, int b, int c){
+
+        while( a>0 && b>0 && c>0 ){
+            if( a%10 + b%10 + c%10 >= 10 ){
+                return false;
+            }
+            a/=10;
+            b/=10;
+            c/=10;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // n개의 수 중 서로다른 3개 골랐을때 carry 되지 않으면서
+        // 나올 수 있는 sum 최댓값 계산
+        // 예) 21 + 35 = 5 6 으로 carry가 일어나지 않음
+        // 모든 경우에서 carry가 발생하면 -1
+
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i ++){
+            for (int j = i +1; j < n; j++){
+                for (int k = j +1; k < n; k++){
+                    if(checkNotCarry(arr[i], arr[j], arr[k])){
+                        max = Math.max(max, arr[i]+arr[j]+arr[k]);
+                    }
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}

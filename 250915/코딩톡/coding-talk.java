600019df8179f53ccddb8d48a304c42652294e56
peng255 <149145441+peng255@users.noreply.github.com>
2025-09-15 18:48:21 +0900

    Title: 코딩톡, Time: 122ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250915/코딩톡/coding-talk.java
+++ 250915/코딩톡/coding-talk.java
@@ -25,8 +25,10 @@ public class Main {
             return;
         }
 
-        for(int i = p-1; i < m; i ++){
-            developers[sender[i]-'A'] ++;
+        for(int i = 0; i < m; i ++){
+            // p번째 이상이거나 .. p번째와 안읽음수가 같다면 읽엇음
+            if(i>=(p-i) || unread[p-1]==unread[i])
+                developers[sender[i]-'A'] ++;
         }
 
         for(int i = 0; i < n; i ++){

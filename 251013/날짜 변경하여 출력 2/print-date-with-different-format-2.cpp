5384ee2d54381adbbf92cb336a1868fb895d4fa5
peng255 <149145441+peng255@users.noreply.github.com>
2025-10-13 23:19:49 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251013/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char temp;
+    int m, d, y;
+    cin >> m >> temp >> d >> temp >> y;
+    cout << y << "." << m << "." << d;
+
+    return 0;
+}

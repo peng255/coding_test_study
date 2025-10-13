0af6c53d96b29397e5b4a0a3f73529d5eab15633
peng255 <149145441+peng255@users.noreply.github.com>
2025-10-13 22:56:45 +0900

    Title: 세 정수형 변수 선언, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251013/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    a = 7;
+    b = 23;
+    c = 30;
+
+    cout << a << " + " << b << " = " << c;
+
+
+}
